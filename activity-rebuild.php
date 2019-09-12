0a1371cb9df8798aa99f913afb23339f0ee9a053
Zeeshan <codezed@icloud.com>
2019-09-12 13:42:56 +0530

    url change to /activity

---
+++ activity-rebuild.php
@@ -0,0 +1,5 @@
+<?php
+
+exec('cd /home/superferry/activity-repo && ./webBuild.sh >&1', $output, $return_var);
+
+?>
--- src/components/header.js
+++ src/components/header.js
@@ -101,7 +101,7 @@ class Header extends Component {
                   </a>
                 </Col>
                 <Col sm={{ size: 4}}  className="header-selected" style={{fontFamily: 'Montserrat', fontSize: '22px', textAlign: 'center' , color: 'white' , backgroundColor: '#1F69B0' , padding: '10px'}}>
-                <a href="/">ACTIVITIES 
+                <a href="/activity">ACTIVITIES 
                     <span style={{fontSize: '14px', marginTop: '-10px', display: 'block'}}>FOR HAVELOCK</span>
                    </a>
                 </Col>
