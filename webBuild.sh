e45dbf536fa5db0b1be4d15568ba6f5bac01cb6d
Zeeshan <codezed@icloud.com>
2019-09-13 17:35:07 +0530

    overwrite cp

--- webBuild.sh
+++ webBuild.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 npm run build-with-prefix-path; 
-rm -rf ./../www/activity/*;
-cp -a ./public/. ./../www/activity/;
+# rm -rf ./../www/activity/*;
+# cp -a ./public/. ./../www/activity/;
+yes | cp -rf ./public/. ./../www/activity/;
